5a60a9ce4e5520235d152c862aa67127cc854156
Gaetz <gaetz@rufflerim.com>
2017-11-01 17:16:54 +0100

    First reducer example

---
+++ src/reducers/reducer_Books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: the good parts'},
+        {title: 'Harry potter'},
+        {title: 'Rue des Macchabées'},
+        {title: 'SFML Game Developement'}
+    ]
+}
