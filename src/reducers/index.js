1532914e94c18fe27ed7f87d302e2c2401c662c7
Gaetz <gaetz@rufflerim.com>
2017-11-04 12:24:42 +0100

    Action wired

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_Books';
+import ActiveBook from './reducer_ActiveBook';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
